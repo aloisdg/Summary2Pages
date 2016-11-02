022345ee21c8a2a32823269a2a7712ed09a8a5ce
aloisdg <aloisdegouvello@live.fr>
2016-11-02 09:14:05 +0100

    Refactor trim logic and style

--- Program.fs
+++ Program.fs
@@ -1,27 +1,27 @@
 ﻿open System
 open System.IO
 
-// use markdig?
-
-let write item = File.AppendAllText(item |> Seq.skip 1 |> Seq.head, "# " + Path.GetFileNameWithoutExtension(item |> Seq.head))
-
-// let createDirectory path : string = Directory.CreateDirectory(Path.GetDirectoryName(path)).FullName
-// let createPath path : string = Path.Combine(createDirectory path, Path.GetFileName(path))
+let write item =
+    File.AppendAllText(item |> Seq.skip 1 |> Seq.head, "# " + Path.GetFileNameWithoutExtension(item |> Seq.head))
 
 // ty @DaveHogan https://github.com/fsharp/FAKE/issues/996
-let create path = //Directory.CreateDirectory(Path.GetDirectoryName(path))
+let create path =
     let directoryName = Path.GetDirectoryName(path)
     if not (System.String.IsNullOrEmpty(directoryName)) then
         Directory.CreateDirectory(directoryName) |> ignore
 
-let isUseful line : bool = System.String.IsNullOrWhiteSpace(string line) = false && (string line).StartsWith("* [") = true && (string line).Contains("](") = true && (string line).EndsWith(")") = true
-
-let trimStart line : string = (string line).Replace("* [", "")
+let isUseful line : bool =
+    System.String.IsNullOrWhiteSpace(string line) = false
+    && (string line).StartsWith("* [") = true
+    && (string line).Contains("](") = true
+    && (string line).EndsWith(")") = true
 
 // (input.[..(input.Length - 2)]) better?
-let trimEnd chars : string = (string chars).Remove((string chars).Length - 1, 1)
+let trim line : string =
+    (string line).Substring(3).Remove((string line).Length - (1 + 3), 1)
 
-let split line : string[] = (string line).Split([|"]("|], StringSplitOptions.RemoveEmptyEntries)
+let split line : string[] =
+    (string line).Split([|"]("|], StringSplitOptions.RemoveEmptyEntries)
 
 [<EntryPoint>]
 let main argv =
@@ -29,12 +29,10 @@ let main argv =
         File.ReadAllLines("SUMMARY.md")
         |> Seq.map (fun x -> (string x).Trim())
         |> Seq.filter isUseful
-        |> Seq.map trimStart
-        |> Seq.map trimEnd
+        |> Seq.map trim
         |> Seq.map split
     for item in x do
         printfn "%A" item
         create (item |> Seq.skip 1 |> Seq.head)
         write item
-    // x |> Seq.map createPath |> Seq.iter write
     0 // return an integer exit code
