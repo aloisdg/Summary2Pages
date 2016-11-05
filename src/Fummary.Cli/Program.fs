995a99956dfad0ac2e2a52b1adbba3c18f555d17
aloisdg <aloisdegouvello@live.fr>
2016-11-05 14:35:59 +0100

    Fix io

--- src/Fummary.Cli/Program.fs
+++ src/Fummary.Cli/Program.fs
@@ -1,6 +1,26 @@
 ﻿open Fummary
+open System
+open System.IO
+
+let write (item : seq<string>) =
+    let fileInfo = new FileInfo(item |> Seq.last)
+    if (not fileInfo.Exists)
+    || fileInfo.Length = (int64 0) then
+        File.AppendAllText(item |> Seq.last, "# " + Path.GetFileNameWithoutExtension(item |> Seq.head))
+
+// ty @DaveHogan https://github.com/fsharp/FAKE/issues/996
+let create (path : string) =
+    let directoryName = Path.GetDirectoryName(path)
+    if not (String.IsNullOrEmpty(directoryName)) then
+        Directory.CreateDirectory(directoryName) |> ignore
 
 [<EntryPoint>]
 let main argv =
-    Fummary.Common.run
+    let lines = File.ReadAllLines("SUMMARY.md") |> Seq.ofArray
+    let summary = Fummary.Common.parse lines
+
+    for item in summary do
+        printfn "%A" item
+        create (item |> Seq.last)
+        write item
     0 // return an integer exit code
--- src/Fummary/Library.fs
+++ src/Fummary/Library.fs
@@ -2,42 +2,27 @@
 
 module Common =
     open System
-    open System.IO
 
-    let delimiter = "]("
-    let head = "* ["
-    let tail = ")"
+    let private delimiter = "]("
+    let private head = "* ["
+    let private tail = ")"
 
-    let write (item : seq<string>) =
-        File.AppendAllText(item |> Seq.last, "# " + Path.GetFileNameWithoutExtension(item |> Seq.head))
-
-    // ty @DaveHogan https://github.com/fsharp/FAKE/issues/996
-    let create (path : string) =
-        let directoryName = Path.GetDirectoryName(path)
-        if not (String.IsNullOrEmpty(directoryName)) then
-         Directory.CreateDirectory(directoryName) |> ignore
-
-    let isUseful (line : string) : bool =
+    let private isUseful (line : string) : bool =
         not (String.IsNullOrWhiteSpace(line))
         && line.StartsWith(head)
         && line.Contains(delimiter)
         && line.EndsWith(tail)
 
     // (input.[..(input.Length - 2)]) better?
-    let trim (line : string) : string =
+    let private trim (line : string) : string =
         line.Substring(head.Length).Remove(line.Length - (tail.Length + head.Length), tail.Length)
 
-    let split (line : string) : string[] =
+    let private split (line : string) : string[] =
         line.Split([|delimiter|], StringSplitOptions.RemoveEmptyEntries)
 
-    let run =
-        let x =
-            File.ReadAllLines("SUMMARY.md")
-            |> Seq.map (fun x -> (string x).Trim())
-            |> Seq.filter isUseful
-            |> Seq.map trim
-            |> Seq.map split
-        for item in x do
-            printfn "%A" item
-            create (item |> Seq.last)
-            write item
+    let parse (lines : seq<string>) : seq<string[]> =
+        lines
+        |> Seq.map (fun x -> (string x).Trim())
+        |> Seq.filter isUseful
+        |> Seq.map trim
+        |> Seq.map split
